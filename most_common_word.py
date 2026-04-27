b38c975509a230f8cecd97095de43c159d8d04e6
Ian Pollack <ianjpollack@gmail.com>
2026-04-26 18:12:13 -0700

    task one done (most_common_word)

--- most_common_word.py
+++ most_common_word.py
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────
 # Task 1 – String & Collections
 # ─────────────────────────────────────────
-def most_common_word(text: str, stopwords: set[str] | None = None) -> str | None:
+def most_common_word(text: str, stopwords: set[str]) -> str | None:
     """
     Return the most frequently occurring word in text,
     ignoring any words in stopwords. Case-insensitive.
@@ -11,4 +11,25 @@ def most_common_word(text: str, stopwords: set[str] | None = None) -> str | None
     # 2. Filter out stopwords (if provided)
     # 3. Count word frequencies
     # 4. Return the most common word, or None if empty
-    pass
+
+    words = text.split(" ")
+    lowercase_stopwords = [stopword.lower() for stopword in stopwords]
+    word_count = {}
+
+    for word in words:
+        word = word.lower()
+        if word not in lowercase_stopwords:
+            if word in word_count.keys():
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+
+    sorted_word_count = dict(sorted(word_count.items(), key=lambda item: item[1], reverse=True))
+    sorted_word_list = list(sorted_word_count.keys())
+
+    if (len(sorted_word_list) > 0):
+        return sorted_word_list[0]
+    else:
+        return None
+
+most_common_word("hello My NAME is Ian helLO HELLO is my my hello", ["is", "MY"])
